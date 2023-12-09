14833d9b4a898e589b8d2f8cce0012211f2f1e65
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2023-12-09 19:00:12 +0500

    shadcn done

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { ModeToggle } from "./Components/ToggleTheme";
+import HomePage from "./Components/Home";
+
+export default function Home() {
+    return (
+        <main>
+            <HomePage />
+        </main>
+    );
+}
